540786820826914c0f27b289c21ceb92a625e38d
Shai Barack <shiber@gmail.com>
2014-12-24 21:31:51 +0200

    queryRoots complete

--- app/src/main/java/com/shaibarack/memer/MemesProvider.java
+++ app/src/main/java/com/shaibarack/memer/MemesProvider.java
@@ -1,8 +1,11 @@
 package com.shaibarack.memer;
 
+import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.CancellationSignal;
 import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
 import android.provider.DocumentsProvider;
 
 import java.io.FileNotFoundException;
@@ -12,6 +15,30 @@ import java.io.FileNotFoundException;
  */
 public class MemesProvider extends DocumentsProvider {
 
+    private static final String[] DEFAULT_ROOT_PROJECTION = new String[]{
+            DocumentsContract.Root.COLUMN_ROOT_ID,
+            DocumentsContract.Root.COLUMN_MIME_TYPES,
+            DocumentsContract.Root.COLUMN_FLAGS,
+            DocumentsContract.Root.COLUMN_ICON,
+            DocumentsContract.Root.COLUMN_TITLE,
+            DocumentsContract.Root.COLUMN_SUMMARY,
+            DocumentsContract.Root.COLUMN_DOCUMENT_ID,
+    };
+
+    private static final String[] DEFAULT_DOCUMENT_PROJECTION = new String[]{
+            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+            DocumentsContract.Document.COLUMN_MIME_TYPE,
+            DocumentsContract.Document.COLUMN_DISPLAY_NAME,
+            DocumentsContract.Document.COLUMN_LAST_MODIFIED,
+            DocumentsContract.Document.COLUMN_FLAGS,
+            DocumentsContract.Document.COLUMN_SIZE
+    };
+
+    private static final int MAX_SEARCH_RESULTS = 20;
+    private static final int MAX_LAST_MODIFIED = 5;
+
+    private static final String ROOT = "root";
+
     @Override
     public boolean onCreate() {
         return true;
@@ -19,7 +46,22 @@ public class MemesProvider extends DocumentsProvider {
 
     @Override
     public Cursor queryRoots(String[] projection) throws FileNotFoundException {
-        return null;
+        MatrixCursor result = new MatrixCursor(resolveRootProjection(projection));
+        MatrixCursor.RowBuilder row = result.newRow();
+        row.add(DocumentsContract.Root.COLUMN_ROOT_ID, ROOT);
+        row.add(DocumentsContract.Root.COLUMN_SUMMARY, getContext().getString(R.string.app_name));
+        row.add(DocumentsContract.Root.COLUMN_FLAGS,
+                DocumentsContract.Root.FLAG_LOCAL_ONLY |
+                DocumentsContract.Root.FLAG_SUPPORTS_RECENTS |
+                DocumentsContract.Root.FLAG_SUPPORTS_SEARCH);
+        row.add(DocumentsContract.Root.COLUMN_TITLE, getContext().getString(R.string.app_name));
+        // We're mirroring a filesystem so column IDs are relative paths and the root is ""
+        row.add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, "");
+        // The child MIME types are used to filter the roots and only present to the user roots
+        // that contain the desired type somewhere in their file hierarchy.
+        row.add(DocumentsContract.Root.COLUMN_MIME_TYPES, "image/*");
+        row.add(DocumentsContract.Root.COLUMN_ICON, R.drawable.ic_launcher);
+        return result;
     }
 
     @Override
@@ -38,5 +80,13 @@ public class MemesProvider extends DocumentsProvider {
     public ParcelFileDescriptor openDocument(String documentId, String mode,
             CancellationSignal signal) throws FileNotFoundException {
         return null;
+    }
+
+    private String[] resolveRootProjection(String[] projection) {
+        return projection != null ? projection : DEFAULT_ROOT_PROJECTION;
+    }
+
+    private static String[] resolveDocumentProjection(String[] projection) {
+        return projection != null ? projection : DEFAULT_DOCUMENT_PROJECTION;
     }
 }
