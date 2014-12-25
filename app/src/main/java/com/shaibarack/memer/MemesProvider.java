7fafc4a49bb8fba8241cc05df325a21393eb4c04
Shai Barack <shiber@gmail.com>
2014-12-25 18:11:01 +0200

    Removed code to deal with directories
    We don't have directories anymore

--- app/src/main/java/com/shaibarack/memer/MemesProvider.java
+++ app/src/main/java/com/shaibarack/memer/MemesProvider.java
@@ -19,9 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import static android.provider.DocumentsContract.Document;
 import static android.provider.DocumentsContract.Root;
@@ -146,34 +144,19 @@ public class MemesProvider extends DocumentsProvider {
             throws FileNotFoundException {
         Log.d("MemesProvider", "querySearchDocuments " + rootId + ", " + query);
 
+        String[] children;
+        try {
+            children = mAssets.list(rootId);
+        } catch (IOException e) {
+            Log.e("MemesProvider", "Failed to list children of " + rootId, e);
+            throw new FileNotFoundException(e.getMessage());
+        }
+
         query = query.toLowerCase();
         MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
-
-        // Iterate through all files in the file structure under the root.
-        Queue<String> pending = new LinkedList<>();
-
-        // Start by adding the parent to the list of files to be processed
-        pending.add(rootId);
-
-        // Do while we still have unexamined files
-        while (!pending.isEmpty()) {
-            // Take a file from the list of unprocessed files
-            String docId = pending.poll();
-            if (isDirectory(docId)) {
-                // If it's a directory, add all its children to the unprocessed list
-                try {
-                    for (String child : mAssets.list(docId)) {
-                        pending.add(docId + File.separator + child);
-                    }
-                } catch (IOException e) {
-                    Log.e("MemesProvider",
-                            "querySearchDocuments failed while listing children", e);
-                }
-            } else {
-                // If it's a file and it matches, add it to the result cursor.
-                if (getFileDisplayName(docId).toLowerCase().contains(query)) {
-                    includeFile(result, docId);
-                }
+        for (String child : children) {
+            if (child.toLowerCase().contains(query)) {
+                includeFile(result, rootId + File.separator + child);
             }
         }
         return result;
@@ -200,16 +183,13 @@ public class MemesProvider extends DocumentsProvider {
     }
 
     private void includeFile(MatrixCursor result, String docId) {
-        boolean isDir = isDirectory(docId);
         result.newRow()
                 .add(Document.COLUMN_DOCUMENT_ID, docId)
-                .add(Document.COLUMN_DISPLAY_NAME,
-                        isDir ? getDirDisplayName(docId) : getFileDisplayName(docId))
+                .add(Document.COLUMN_DISPLAY_NAME, getFileDisplayName(docId))
                 .add(Document.COLUMN_SIZE, null)
-                .add(Document.COLUMN_MIME_TYPE, isDir ? Document.MIME_TYPE_DIR : MIME_TYPE_IMAGE)
+                .add(Document.COLUMN_MIME_TYPE, MIME_TYPE_IMAGE)
                 .add(Document.COLUMN_LAST_MODIFIED, null)
-                .add(Document.COLUMN_FLAGS,
-                        isDir ? Document.FLAG_DIR_PREFERS_GRID : Document.FLAG_SUPPORTS_THUMBNAIL);
+                .add(Document.COLUMN_FLAGS, Document.FLAG_SUPPORTS_THUMBNAIL);
     }
 
     private List<String> getRecents() {
@@ -227,14 +207,6 @@ public class MemesProvider extends DocumentsProvider {
         }
         String toPref = TextUtils.join(",", recents);
         mPrefs.edit().putString(RECENTS_KEY, toPref).commit();
-    }
-
-    private static boolean isDirectory(String docId) {
-        // AssetManager doesn't offer an easy way to determine if a path is an asset directory or an
-        // asset file. This is in line with the general understanding that you're expected to know
-        // what is in your assets.
-        // Instead we rely on this hack:
-        return !docId.contains(".");
     }
 
     /**
